2cbaec43196461c44d952380e1fba434aa0978d1
coderanant <coderanant@gmail.com>
2019-06-07 21:51:26 +0530

    CodeForces

---
+++ CodeForces/Codeforces Round 564/B.cpp
@@ -0,0 +1,46 @@
+/*coderanant*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define f1(i,a,b) for(i=a;i<b;i++)
+#define f2(i,a,b) for(i=a;i>=b;i--)
+#define endl '\n'
+#define pb push_back
+#define gp " "
+#define ff first
+#define ss second
+#define mp make_pair
+
+const int mod=1000000007;
+int i,j;
+ll temp;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    	freopen("/home/akmittal/Desktop/Competitive Programming/in.txt","r",stdin);
+    	freopen("/home/akmittal/Desktop/Competitive Programming/out.txt","w",stdout);
+    #endif
+    int n;
+    cin>>n;
+    int m=(n+2)/2;
+    cout<<m<<endl;
+    int r=1,c=1;
+    f1(i,0,n)
+    {
+    	cout<<r<<gp<<c<<endl;
+    	if(r==c)
+    	{
+    		c++;
+    	}
+    	else
+    	{	
+    		r++;
+    	}
+    }
+    return 0;
+}
