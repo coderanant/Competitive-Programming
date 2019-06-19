a27d34ef01d783569e5174f2ad473c5115a60d4c
coderanant <coderanant@gmail.com>
2019-06-19 13:43:50 +0530

    Heuristics 31

---
+++ Other Contests/Heuristics 31/A.cpp
@@ -0,0 +1,41 @@
+/*coderanant*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define f1(i,a,b) for(i=a;i<b;i++)
+#define f2(i,a,b) for(i=a;i>=b;i--)
+#define endl '\n'
+#define pb push_back
+#define gp " "
+#define ff first
+#define ss second
+#define mp make_pair
+
+const int mod=1000000007;
+int i,j;
+ll temp;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    	freopen("/home/akmittal/Desktop/Competitive Programming/in.txt","r",stdin);
+    	freopen("/home/akmittal/Desktop/Competitive Programming/out.txt","w",stdout);
+    #endif
+    int n;
+    int ans;
+    cin>>n;
+    n--;
+    cin>>ans;
+    int temp;
+    for(int i=0;i<n;i++)
+    {
+    	cin>>temp;
+    	ans=(ans*temp)/__gcd(ans,temp);
+    }
+    cout<<ans;
+    return 0;
+}
