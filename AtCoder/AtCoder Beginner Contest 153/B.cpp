0cadb7e5f6ff9b29f102c4842645205b91688236
coderanant <coderanant@gmail.com>
2020-01-29 23:54:37 +0530

    AtCoder

---
+++ AtCoder/AtCoder Beginner Contest 153/B.cpp
@@ -0,0 +1,44 @@
+/*coderanant*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define f1(i,a,b) for(int i=a;i<b;i++)
+#define f2(i,a,b) for(int i=a;i>=b;i--)
+#define endl '\n'
+#define pb push_back
+#define gp " "
+#define ff first
+#define ss second
+#define mp make_pair
+
+const int mod=1000000007;
+ll temp;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // #ifndef ONLINE_JUDGE
+    // 	freopen("/home/akmittal/Desktop/Competitive Programming/in.txt","r",stdin);
+    // 	freopen("/home/akmittal/Desktop/Competitive Programming/out.txt","w",stdout);
+    // #endif
+
+    int h,n;
+    cin>>h>>n;
+    int sum=0;
+    f1(i,0,n)
+    {
+    	cin>>temp;
+    	sum+=temp;
+    }
+    if(sum>=h)
+    {
+    	cout<<"Yes"<<endl;
+    }
+    else
+    	cout<<"No"<<endl;
+    return 0;
+}
