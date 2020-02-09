d3f6f9f46e0e020a2d41d26d43a6d7fca68c6598
coderanant <coderanant@gmail.com>
2020-02-09 22:28:42 +0530

    Codeforces

---
+++ CodeForces/Codeforces Round 618/B.cpp
@@ -0,0 +1,45 @@
+/*coderanant*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define f1(i,a,b) for(int i=a;i<b;i++)
+#define f2(i,a,b) for(int i=a;i>=b;i--)
+#define endl '\n'
+#define pb push_back
+#define gp " "
+#define ff first
+#define ss second
+#define mp make_pair
+
+const int mod=1000000007;
+ll temp;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    #ifndef ONLINE_JUDGE
+    	freopen("/home/akmittal/Desktop/Competitive Programming/in.txt","r",stdin);
+    	freopen("/home/akmittal/Desktop/Competitive Programming/out.txt","w",stdout);
+    #endif
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	int n;
+    	cin>>n;
+    	n*=2;
+    	int a[n];
+    	f1(i,0,n)
+    	cin>>a[i];
+
+    	sort(a,a+n);
+    	
+    	cout<<a[n/2]-a[n/2-1]<<endl;
+    }
+    return 0;
+}
