956e5ee90f2fb0fa0c8f7de410e8b19a29145589
coderanant <coderanant@gmail.com>
2020-04-05 13:06:57 +0530

    Atcoder

---
+++ AtCoder/AtCoder Beginner Contest 161/A.cpp
@@ -0,0 +1,27 @@
+/*coderanant*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define f1(i,a,b) for(int i=a;i<b;i++)
+#define f2(i,a,b) for(int i=a;i>=b;i--)
+#define endl '\n'
+#define pb push_back
+#define gp " "
+#define ff first
+#define ss second
+#define mp make_pair
+
+const int mod=1000000007;
+ll temp;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int x,y,z;
+    cin>>x>>y>>z;
+    cout<<z<<gp<<x<<gp<<y<<endl;
+    return 0;
+}
