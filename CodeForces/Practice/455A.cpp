dcd0f4b02322bafad5b95851938c04121e37d9fb
coderanant <coderanant@gmail.com>
2020-04-12 13:20:18 +0530

    Codeforces

---
+++ CodeForces/Practice/455A.cpp
@@ -0,0 +1,41 @@
+/*coderanant*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define f1(i,a,b) for(int i=a;i<b;i++)
+#define f2(i,a,b) for(int i=a;i>=b;i--)
+#define endl '\n'
+#define pb push_back
+#define gp " "
+#define ff first
+#define ss second
+#define mp make_pair
+
+const int mod=1000000007;
+ll temp;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin>>n;
+    int a[100001];
+    memset(a, 0, sizeof(a));
+    f1(i,0,n)
+    {
+        cin>>temp;
+        a[temp]++;
+    }
+
+    f1(i,2,100001)
+    {
+        a[i] = max(a[i-2]+a[i]*i, a[i-1]);
+    }
+
+    cout<<a[100000]<<endl;
+    return 0;
+}
