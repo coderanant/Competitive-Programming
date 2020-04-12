d7612200a1435aa71450c5e2ebc6faa94fbdd89f
coderanant <coderanant@gmail.com>
2020-04-12 17:43:47 +0530

    Codeforces

---
+++ CodeForces/Practice/282C.cpp
@@ -0,0 +1,52 @@
+/*coderanant*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define f1(i,a,b) for(int i=a;i<b;i++)
+#define f2(i,a,b) for(int i=a;i>=b;i--)
+#define endl '\n'
+#define pb push_back
+#define gp " "
+#define ff first
+#define ss second
+#define mp make_pair
+
+const int mod=1000000007;
+ll temp;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string a,b;
+    cin>>a>>b;
+    int n = a.size();
+    if(a.size() != b.size())
+    {
+        cout<<"NO"<<endl;
+        return 0;
+    }
+    
+    int as = 0, bs = 0;
+    f1(i,0,n)
+    {
+        if(a[i] == '0') as++;
+        if(b[i] == '0') bs++;
+    }
+    if(as == n && bs != n)
+    {
+        cout<<"NO"<<endl;
+        return 0;
+    }
+
+    if(as != n && bs == n)
+    {
+        cout<<"NO"<<endl;
+        return 0;
+    }
+
+    cout<<"YES"<<endl;
+    return 0;
+}
