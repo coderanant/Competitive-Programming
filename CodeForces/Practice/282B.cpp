b582e7dadb40ff9cca85fe105f578cfaef99f803
coderanant <coderanant@gmail.com>
2020-04-13 17:46:05 +0530

    Codeforces

--- CodeForces/Practice/282B.cpp
+++ CodeForces/Practice/282B.cpp
@@ -12,7 +12,7 @@ using namespace std;
 #define ff first
 #define ss second
 #define mp make_pair
-#define ppp pair<pair<int,int>, int>
+
 const int mod=1000000007;
 ll temp;
 
@@ -22,57 +22,21 @@ int32_t main()
     cin.tie(NULL);
     int n;
     cin>>n;
-
-    vector<ppp> a(n+1);
-    f1(i,1,n+1)
+    int sum = 0;
+    f1(i,0,n)
     {
-        cin>>a[i].ff.ff>>a[i].ff.ss;
-        a[i].ss = i;
-    }
-
-    sort(a.begin(), a.end());
-    int sumf = 0, las = 0;
-
-    f1(i,1,n+1)
-    las += a[i].ff.ss;
-
-    int ind = -1;
-    if(abs(sumf-las) <= 500)
-    {
-        ind = 0;
-    }
-    else
-    {
-        f1(i,1,n+1)
+        int a, b;
+        cin>>a>>b;
+        if(sum + a <= 500)
         {
-            sumf += a[i].ff.ff;
-            las -= a[i].ff.ss;
-            if( abs(sumf - las) <= 500)
-            {
-                ind = i;
-                break;
-            }
-        }
-    }
-
-    if(ind == -1)
-    cout<<-1<<endl;
-    else
-    {
-        string ans;
-        ans.resize(n+1);
-        f1(i,1,ind+1)
-        {
-            ans[a[i].ss] = 'A';
-        }
-        f1(i,1,n+1)
-        {
-            if(ans[i]!='A')
-            cout<<'G';
-            else
+            sum += a;
             cout<<'A';
         }
+        else
+        {
+            sum -= b;
+            cout<<'G';
+        }
     }
-    
     return 0;
 }
