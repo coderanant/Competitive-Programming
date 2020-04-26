c331bfc76856202e470dd11ca5abcf3cf3c736eb
coderanant <coderanant@gmail.com>
2020-04-26 23:35:53 +0530

    Codeforces

---
+++ CodeForces/Educational Codeforces Round 86/A.cpp
@@ -0,0 +1,37 @@
+/*coderanant*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define f1(i,a,b) for(int i=a;i<b;i++)
+#define f2(i,a,b) for(int i=a;i>=b;i--)
+#define endl '\n'
+#define pb push_back
+#define gp " "
+#define ff first
+#define ss second
+#define mp make_pair
+
+const int mod=1000000007;
+ll temp;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x, y;
+        cin>>x>>y;
+        int a, b;
+        cin>>a>>b;
+
+        int ans = abs(x-y)*a;
+        ans += min(min(x,y)*2*a, min(x,y)*b);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
