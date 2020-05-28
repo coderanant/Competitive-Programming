74fb29f5a331a3298d5fb3c6fe90298363d04d48
coderanant <coderanant@gmail.com>
2020-05-28 16:40:46 +0530

    Codeforces

---
+++ CodeForces/Educational Codeforces Round 87/C2.cpp
@@ -0,0 +1,33 @@
+/*coderanant*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define f1(i,a,b) for(int i=a;i<b;i++)
+#define f2(i,a,b) for(int i=a;i>=b;i--)
+#define endl '\n'
+#define pb push_back
+#define gp " "
+#define ff first
+#define ss second
+#define mp make_pair
+
+const int mod=1000000007;
+ll temp;
+const long double PI = acosl(-1.0);
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        long double ans = cos(PI / (4 * n)) / sin(PI / (2 * n));
+        cout<<setprecision(10)<<fixed<<ans<<endl;
+    }
+    return 0;
+}
