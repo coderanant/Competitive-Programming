6623a4051c74f128c5a3b77641238ea704a2c4c6
coderanant <coderanant@gmail.com>
2020-08-30 12:45:22 +0530

    Atcoder

---
+++ AtCoder/AtCoder Beginner Contest 177/A.cpp
@@ -0,0 +1,32 @@
+/*coderanant*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define f1(i,a,b) for(int i=a;i<b;i++)
+#define f2(i,a,b) for(int i=a;i>=b;i--)
+#define endl '\n'
+#define pb push_back
+#define gp " "
+#define ff first
+#define ss second
+#define mp make_pair
+#define ld long double
+
+const int mod = 1000000007;
+const ld PI = acosl(-1.0);
+ll temp;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int d, t, s;
+    cin>>d>>t>>s;
+    if((d + s - 1) / s <= t)
+    cout<<"Yes"<<endl;
+    else
+    cout<<"No"<<endl;
+    return 0;
+}
