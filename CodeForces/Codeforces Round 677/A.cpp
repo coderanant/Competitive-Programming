fc687335d6d78dadb4c430e8108e6d4db0106345
coderanant <coderanant@gmail.com>
2020-10-21 17:48:44 +0530

    Codeforces

---
+++ CodeForces/Codeforces Round 677/A.cpp
@@ -0,0 +1,43 @@
+/*coderanant*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define f1(i,a,b) for(int i=a;i<b;i++)
+#define f2(i,a,b) for(int i=a;i>=b;i--)
+#define endl '\n'
+#define pb push_back
+#define gp " "
+#define ff first
+#define ss second
+#define mp make_pair
+#define ld long double
+
+const int mod = 1000000007;
+const ld PI = acosl(-1.0);
+ll temp;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int x = n % 10;
+        int dig = 0;
+        while(n)
+        {
+            dig++;
+            n /= 10;
+        }
+        int ans = (x - 1) * 10;
+        ans += (dig) * (dig + 1) / 2;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
